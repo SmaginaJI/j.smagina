25a3eb8455a29f5841d4984833081ab67cffe84f
SmaginaJI <smaginaji@gmail.com>
2016-04-12 15:56:58 +0600

    1

---
+++ Example1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int res = (n) & (int.MaxValue-1);
+            Console.WriteLine(res);
+            
+            /* task2
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = Convert.ToInt32(Console.ReadLine());
+            int res = (n>>i)&1;
+            Console.WriteLine(res);*/
+
+            /* task1
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int sum = n + m;
+            Console.WriteLine(sum);*/
+        }
+    }
+}
