8d64696eb4faafebd84a7378dd9c61244e57ef0b
SmaginaJI <smaginaji@gmail.com>
2016-04-28 08:56:16 +0600

    method GetPhone()

---
+++ Lesson4/Phone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Phone
+    {
+        private string _CityCode;
+        private string _PhoneNumber;
+
+        public Phone(string cityCode, string phoneNumber)
+        {
+            _CityCode = cityCode;
+            _PhoneNumber = phoneNumber;
+        }
+
+        public string GetPhone()
+        {
+            if (_CityCode == string.Empty || _CityCode == null)
+            {
+                return _PhoneNumber;
+            }
+            else
+            {
+                return string.Format("({0}) {1}", _CityCode, _PhoneNumber);
+            }
+        }
+
+    }
+}
---
+++ Lesson4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "";
+            string y = "";
+            x = Console.ReadLine();
+            y = Console.ReadLine();
+            Phone Number1 = new Phone(x, y);
+            Console.WriteLine(Number1.GetPhone());
+        }
+    }
+}
