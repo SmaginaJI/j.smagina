d967ddf42a34dd293f9fff717561d087c1abd327
SmaginaJI <smaginaji@gmail.com>
2016-04-28 20:12:36 +0600

    ?

--- Example1/Properties/AssemblyInfo.cs
+++ Example1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Example1")]
+[assembly: AssemblyTitle("Lesson1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Example1")]
+[assembly: AssemblyProduct("Lesson1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
