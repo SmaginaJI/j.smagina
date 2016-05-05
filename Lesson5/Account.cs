03796b64e1d8bb7c5b0c8fc49315c36bc0c57f3b
SmaginaJI <smaginaji@gmail.com>
2016-05-05 14:39:32 +0600

    lesson 5 task 2 (accounts)

---
+++ Lesson5/Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    class Account
+    {
+        public Guid AccountID { get; readonly set; }
+        public string Status { get; private set; }
+        public string Owner { get; protected set; }
+        public decimal Amount { get; protected set; }
+
+        protected Account(string lastName, string firstName)
+        {
+            Owner = lastName + firstName;
+            AccountID = Guid.NewGuid();
+            Status = "open";
+        }
+
+        protected bool CloseAccount()
+        {
+            if (Amount == 0)
+            {
+                Status = "closed";
+                return true;
+            }
+            else return false;
+        }
+
+        protected bool DepositMoney(decimal sum)
+        {
+            Amount = Amount + sum;
+            return true;
+        }
+        protected bool WithdrawMoney(decimal sum)
+        {
+            if (Amount >= sum)
+            {
+                Amount = Amount - sum;
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Lesson5/DepositAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    class DepositAccount: Account
+    {
+        public DepositAccount(string lastName, string firstName)
+            : base(lastName, firstName)
+        {
+        }
+    }
+}
